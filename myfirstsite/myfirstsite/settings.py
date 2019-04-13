df3b695ee38cc89c12049160dce2c26b1e0c6702
DanyaYashin <yashindan2000@gmail.com>
2019-04-13 12:55:25 +0400

    template for post detail

--- myfirstsite/myfirstsite/settings.py
+++ myfirstsite/myfirstsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1wg0dz*1!v-$i8zhn+w6$pag_-c+^0l)oxpnrkisz0r%x*0z!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
