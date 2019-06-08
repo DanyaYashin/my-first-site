21dca0c59ad7bb463d9776131a0751c922e04d52
DanyaYashin <yashindan2000@gmail.com>
2019-06-08 15:10:33 +0400

    add choice

--- myfirstsite/polls/admin.py
+++ myfirstsite/polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Question
-admin.site.register(Question)
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model=Choice
+    extra=1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets=[(None,{'fields':['question_text']}),('Date/Time info',{'fields':['pub_date']})]
+    inlines=[ChoiceInline]
+    list_display=('question_text','pub_date','was_published_recently')
+    list_filter=['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
