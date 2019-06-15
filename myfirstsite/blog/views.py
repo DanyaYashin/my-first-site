de1832ddac8c0c9ebd264450af15ed843a5ee885
DanyaYashin <yashindan2000@gmail.com>
2019-06-15 13:24:08 +0400

    polls base

--- myfirstsite/blog/views.py
+++ myfirstsite/blog/views.py
@@ -54,7 +54,7 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method=="POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -64,10 +64,11 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html',{'form':form})
+
 def post_edit(request, pk):
     post=get_object_or_404(Post, pk=pk)
     if request.method=="POST":
-        form=PostForm(request.POST, instance=post)
+        form=PostForm(request.POST,request.FILES, instance=post)
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
